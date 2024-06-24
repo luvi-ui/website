dd04efb68895768faf380cd9cde6fbf3ff721959
johannes <4764281+anotherfrontendguy@users.noreply.github.com>
2024-06-24 11:54:12 +0200

    document link usage

---
+++ resources/views/pages/docs/components/link.blade.php
@@ -0,0 +1,11 @@
+<x-layouts.docs>
+    <div>
+        <x-docs.component-info
+            name="Link"
+            description="Renders an anchor tag."
+        />
+        <x-preview name="link" />
+        <x-installation name="link" />
+        <x-usage name="link" />
+    </div>
+</x-layouts.docs>
