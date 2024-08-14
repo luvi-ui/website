f22abd077f3701af97ca37b5420caba382982c9e
johannes <4764281+anotherfrontendguy@users.noreply.github.com>
2024-08-14 12:00:30 +0200

    add sortable dummy

---
+++ resources/views/components/local/demo-tasks/sortable/index.blade.php
@@ -0,0 +1,24 @@
+<x-dropdown-menu>
+    <x-dropdown-menu.trigger
+        variant="ghost"
+        size="sm"
+        class="-ml-3 h-8"
+    >
+        {{ $slot }} <x-lucide-chevrons-up-down class="ml-2 size-4" />
+    </x-dropdown-menu.trigger>
+    <x-dropdown-menu.content>
+        <x-dropdown-menu.item>
+            <x-lucide-arrow-up class="mr-2 size-3.5 text-muted-foreground/70" />
+            Asc
+        </x-dropdown-menu.item>
+        <x-dropdown-menu.item>
+            <x-lucide-arrow-down class="mr-2 size-3.5 text-muted-foreground/70" />
+            Desc
+        </x-dropdown-menu.item>
+        <x-dropdown-menu.separator />
+        <x-dropdown-menu.item>
+            <x-lucide-eye-off class="mr-2 size-3.5 text-muted-foreground/70" />
+            Hide
+        </x-dropdown-menu.item>
+    </x-dropdown-menu.content>
+</x-dropdown-menu>
--- resources/views/livewire/demo-tasks/task.blade.php
+++ resources/views/livewire/demo-tasks/task.blade.php
@@ -1,3 +1,29 @@
+@php
+    $tasks = [
+        (object) [
+            'id' => 'TASK-8782',
+            'title' => "You can't compress the program without quantifying the open-source SSD pixel!",
+            'status' => 'in progress',
+            'label' => 'documentation',
+            'priority' => 'medium',
+        ],
+        (object) [
+            'id' => 'TASK-7878',
+            'title' => 'Try to calculate the EXE feed, maybe it will index the multi-byte pixel!',
+            'status' => 'backlog',
+            'label' => 'documentation',
+            'priority' => 'medium',
+        ],
+        (object) [
+            'id' => 'TASK-7839',
+            'title' => 'We need to bypass the neural TCP card!',
+            'status' => 'todo',
+            'label' => 'bug',
+            'priority' => 'high',
+        ],
+    ];
+@endphp
+
 <div class="space-y-4">
     <div class="rounded-md border">
         <x-table>
@@ -5,26 +31,63 @@
                 <x-table.row>
                     <x-table.head><x-checkbox /></x-table.head>
                     <x-table.head>Task</x-table.head>
-                    <x-table.head>Title</x-table.head>
-                    <x-table.head>Status</x-table.head>
-                    <x-table.head>Priority</x-table.head>
+                    <x-table.head>
+                        <x-local.demo-tasks.sortable>
+                            Title
+                        </x-local.demo-tasks.sortable>
+                    </x-table.head>
+                    <x-table.head>
+                        <x-local.demo-tasks.sortable>
+                            Status
+                        </x-local.demo-tasks.sortable>
+                    </x-table.head>
+                    <x-table.head>
+                        <x-local.demo-tasks.sortable>
+                            Priority
+                        </x-local.demo-tasks.sortable>
+                    </x-table.head>
                     <x-table.head>{{-- dropdown menu --}}</x-table.head>
                 </x-table.row>
             </x-table.header>
             <x-table.body>
-                <x-table.row>
-                    <x-table.cell><x-checkbox /></x-table.cell>
-                    <x-table.cell>TASK-8782</x-table.cell>
-                    <x-table.cell>
-                        <x-badge variant="outline">Documentation</x-badge>
-                        Try to calculate the EXE feed, maybe it will index the multi-byte pixel!
-                    </x-table.cell>
-                    <x-table.cell>Backlog</x-table.cell>
-                    <x-table.cell>Medium</x-table.cell>
-                    <x-table.cell>
-                        <x-lucide-ellipsis class="size-4 text-gray-400" />
-                    </x-table.cell>
-                </x-table.row>
+                @foreach ($tasks as $task)
+                    <x-table.row>
+                        <x-table.cell><x-checkbox /></x-table.cell>
+                        <x-table.cell>{{ $task->id }}</x-table.cell>
+                        <x-table.cell>
+                            <div class="flex space-x-2">
+                                <x-badge variant="outline">{{ $task->label }}</x-badge>
+                                <span class="max-w-[500px] truncate font-medium">
+                                    {{ $task->title }}
+                                </span>
+                            </div>
+                        </x-table.cell>
+                        <x-table.cell>
+                            <div class="flex w-[100px] items-center">
+                                <x-lucide-timer class="mr-2 size-4 text-muted-foreground" />
+                                <span>{{ $task->status }}</span>
+                            </div>
+                        </x-table.cell>
+                        <x-table.cell>
+                            <div class="flex items-center">
+                                <x-lucide-arrow-right class="mr-2 size-4 text-muted-foreground" />
+                                <span>{{ $task->priority }}</span>
+                            </div>
+                        </x-table.cell>
+                        <x-table.cell>
+                            <x-dropdown-menu>
+                                <x-dropdown-menu.trigger variant="ghost">
+                                    <x-lucide-ellipsis class="size-4 text-gray-400" />
+                                </x-dropdown-menu.trigger>
+                                <x-dropdown-menu.content>
+                                    <x-dropdown-menu.item>Edit</x-dropdown-menu.item>
+                                    <x-dropdown-menu.item>Make a copy</x-dropdown-menu.item>
+                                    <x-dropdown-menu.item>Favorite</x-dropdown-menu.item>
+                                </x-dropdown-menu.content>
+                            </x-dropdown-menu>
+                        </x-table.cell>
+                    </x-table.row>
+                @endforeach
             </x-table.body>
         </x-table>
     </div>
