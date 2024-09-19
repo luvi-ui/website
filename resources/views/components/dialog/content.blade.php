79f1b72010e469ee6a387e11aec803db72e7ce22
johannes <4764281+anotherfrontendguy@users.noreply.github.com>
2024-09-19 14:06:02 +0200

    add close button

--- resources/views/components/dialog/content.blade.php
+++ resources/views/components/dialog/content.blade.php
@@ -3,4 +3,14 @@
     {{ $attributes->twMerge('w-full max-w-lg border bg-background p-6 shadow-lg  sm:rounded-lg backdrop:bg-black/80') }}
 >
     {{ $slot }}
+
+
+    <button
+        class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+        variant="ghost"
+        size="icon"
+        @click="$refs.__dialog.close()"
+    >
+        <x-lucide-x class="size-4" />
+    </button>
 </dialog>
