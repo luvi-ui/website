eab9ae9426d8a368c9935bd8b4597680d71898cc
johannes <4764281+anotherfrontendguy@users.noreply.github.com>
2024-09-20 16:11:03 +0200

    format class names

--- resources/views/components/dialog/content.blade.php
+++ resources/views/components/dialog/content.blade.php
@@ -1,22 +1,6 @@
 <dialog
     x-ref="__dialog"
-    {{ $attributes->twMerge('w-full max-w-lg border bg-background p-6 shadow-lg  sm:rounded-lg
-        transition-[translate,opacity,scale,overlay,display]
-        [transition-behavior:allow-discrete]
-        open:animate-in
-        open:fade-in-0
-        open:zoom-in-95
-        animate-out
-        duration-200
-        fade-out-0
-        zoom-out-95
-        backdrop:bg-black/80
-        backdrop:duration-300
-        backdrop:opacity-0
-        backdrop:transition-[opacity,display,overlay]
-        backdrop:[transition-behavior:allow-discrete]
-        open:backdrop:opacity-100
-        [@starting-style]:open:backdrop:opacity-0') }}
+    {{ $attributes->twMerge('w-full max-w-lg border bg-background p-6 shadow-lg  sm:rounded-lg transition-[translate,opacity,scale,overlay,display] [transition-behavior:allow-discrete] open:animate-in open:fade-in-0 open:zoom-in-95 animate-out duration-200 fade-out-0 zoom-out-95 backdrop:bg-black/80 backdrop:duration-300 backdrop:opacity-0 backdrop:transition-[opacity,display,overlay] backdrop:[transition-behavior:allow-discrete] open:backdrop:opacity-100 [@starting-style]:open:backdrop:opacity-0') }}
 >
     {{ $slot }}
 
