bb814eb8aaeee92e0b69ea468e85624ae64b2b43
johannes <4764281+anotherfrontendguy@users.noreply.github.com>
2024-09-23 12:25:03 +0200

    WIP dialog docs

--- resources/views/pages/docs/components/dialog.blade.php
+++ resources/views/pages/docs/components/dialog.blade.php
@@ -5,14 +5,22 @@
             description="A window overlaid on either the primary window or another dialog window, rendering the content underneath inert."
         />
 
-        <x-typography.p>Quick Note on browser compatibility:</x-typography.p>
-        <x-typography.p>This component makes use of two modern CSS
-            features: <x-link
-                href="https://caniuse.com/?search=allow-discrete">transition-behavior:allow-discrete</x-link> and <x-link
-                href="https://developer.mozilla.org/en-US/docs/Web/CSS/@starting-style"
-            >@starting-style</x-link>. Unfortunately both are not yet supported in Firefox. The component will still
-            work but it will be missing the enter & exit animations.
+        <x-typography.p>Quick Note on browser compatibility.</x-typography.p>
+        <x-typography.p>This component makes use of two modern CSS features:
+
+            <x-typography.list>
+                <li>
+                    <x-link
+                        href="https://developer.mozilla.org/en-US/docs/Web/CSS/@starting-style">@starting-style</x-link>
+                </li>
+                <li>
+                    <x-link href="https://caniuse.com/?search=allow-discrete">transition-behavior:allow-discrete</x-link>
+                </li>
+            </x-typography.list>
+            Unfortunately both are not (yet) supported in Firefox. The component will still
+            work as expected but it will be missing the enter & exit animations.
         </x-typography.p>
+
         <x-preview name="dialog" />
         <x-installation name="dialog" />
         <x-usage name="dialog" />
