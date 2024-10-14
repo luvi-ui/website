a68125f3ff84139437676d2411e527b3444763d9
johannes <4764281+anotherfrontendguy@users.noreply.github.com>
2024-10-14 12:41:51 +0200

    WIP toast

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,31 @@
 import collapse from "@alpinejs/collapse";
 import anchor from "@alpinejs/anchor";
 
-document.addEventListener(
-  "alpine:init",
-  () => {
-    const modules = import.meta.glob("./plugins/**/*.js", { eager: true });
+let count = 1;
+const idMap = new WeakMap();
 
-    for (const path in modules) {
-      window.Alpine.plugin(modules[path].default);
+window.getObjectId = (object) => {
+    const objectId = idMap.get(object);
+    if (objectId === undefined) {
+        count += 1;
+        idMap.set(object, count);
+
+        return count;
     }
-    window.Alpine.plugin(collapse);
-    window.Alpine.plugin(anchor);
-  },
-  { once: true },
+
+    return objectId;
+};
+
+document.addEventListener(
+    "alpine:init",
+    () => {
+        const modules = import.meta.glob("./plugins/**/*.js", { eager: true });
+
+        for (const path in modules) {
+            window.Alpine.plugin(modules[path].default);
+        }
+        window.Alpine.plugin(collapse);
+        window.Alpine.plugin(anchor);
+    },
+    { once: true },
 );
--- resources/views/components/toast/index.blade.php
+++ resources/views/components/toast/index.blade.php
@@ -1,5 +1,6 @@
 @php
     $styles = implode(' ', [
+        'duration-1000',
         'group',
         'pointer-events-auto',
         'relative',
@@ -21,9 +22,9 @@
 @endphp
 
 <dialog
-    id="toast"
     popover=manual
     class="bottom-0 left-auto right-0 top-auto w-full max-w-[420px] p-4 space-y-4 overflow-hidden"
+    :class="{ 'p-4': toasts.length > 0 }"
     open
     x-data="{
         toasts: [],
@@ -35,26 +36,38 @@
             if (typeof toast.slots.action === 'string') {
                 toast.slots.action = $wire[toast.slots.action];
             }
+
+            toast.id = `toast-${window.getObjectId(toast)}`;
             this.toasts.push(toast);
+
+            toast.timerId = setTimeout(async () => {
+                const toastEl = this.$el.querySelector(`#${toast.id}`);
+                console.log(toastEl);
+                toastEl.classList.add('animate-out', 'slide-out-to-bottom', 'fill-mode-forwards', 'fade-out');
+            }, toast.duration);
         },
 
-        removeToastfromDOM({ animationName }) {
-            if (animationName === 'exit') this.$el.parentElement.remove();
+        removeToast({ animationName }, toast) {
+            if (animationName === 'exit') {
+                this.toasts.splice(this.toasts.indexOf(toast), 1);
+            }
         },
 
         showToasts(toasts) {
             toasts.forEach(toast => this.showToast(toast))
         },
     }"
-    x-id="['toast']"
     x-on:toast-show.document="showToast(event.detail)"
 >
-    <template x-for="toast in toasts">
+    <template
+        x-for="toast in toasts"
+        :key="toast.id"
+    >
         <div>
             <div
+                :id="toast.id"
                 class="{{ $styles }}"
-                x-on:click="$el.classList.add('animate-out', 'slide-out-to-bottom', 'fill-mode-forwards', 'fade-out');"
-                x-on:animationend="removeToastfromDOM($event)"
+                x-on:animationend="removeToast($event, toast)"
             >
                 <div class="grid gap-1">
                     <div
