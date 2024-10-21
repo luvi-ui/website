3c8c8b6d7bab3bbf4a8d47cacd3bccdc72c08e3d
johannes <4764281+anotherfrontendguy@users.noreply.github.com>
2024-10-21 12:16:44 +0200

    WIP

--- resources/views/components/form/message.blade.php
+++ resources/views/components/form/message.blade.php
@@ -1,4 +1,5 @@
 @aware(['name'])
+
 @props([
     'name' => '',
 ])
--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -1,3 +1,72 @@
 <div>
-    playground
+
+    @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <x-form wire:submit.prevent="save">
+
+        <x-form.item>
+            <x-form.label>Username</x-form.label>
+            <x-input
+                x-form:control
+                placeholder="shadcn"
+            />
+            <x-form.description>
+                This is your public display name. It can be your real name or a
+                pseudonym. You can only change this once every 30 days.
+            </x-form.description>
+            <x-form.message />
+        </x-form.item>
+
+        <x-form.item>
+            <x-form.label>Email</x-form.label>
+            <x-select x-form:control>
+                <option
+                    value="Select a verified email to display"
+                    disabled
+                >
+                    Select a verified email to display </option>
+                <option value="m@example.com">m@example.com</option>
+                <option value="m@google.com">m@google.com</option>
+                <option value="m@support.com">m@support.com</option>
+            </x-select>
+            <x-form.description>
+                You can manage verified email addresses in your
+                <x-link href="/examples/forms">email settings</x-link>.
+            </x-form.description>
+            <x-form.message />
+        </x-form.item>
+
+        <x-form.item name="form.bio">
+            <x-form.label>Bio</x-form.label>
+            <x-textarea
+                wire:model="bio"
+                x-form:control
+                placeholder="Tell us a little bit about yourself"
+                class="resize-none"
+            />
+            <x-form.description>
+                You can <span>@mention</span> other users and organizations to
+                link to them.
+            </x-form.description>
+            <x-form.message />
+        </x-form.item>
+
+
+        <x-new-input
+            label="Testing"
+            wire:model="form.test"
+            placeholder="testing"
+            description-trailing="This is your public display name. It can be your real name or a pseudonym. You can only change this once every 30 days."
+        />
+
+        <x-button type="submit">Update profile</x-button>
+    </x-form>
 </div>
