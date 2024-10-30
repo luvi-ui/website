c17d6af5b46f8ac14777ac3b973ad60cd920587b
johannes <4764281+anotherfrontendguy@users.noreply.github.com>
2024-10-30 09:14:21 +0100

    use DialogCvaService

--- resources/views/components/dialog/content.blade.php
+++ resources/views/components/dialog/content.blade.php
@@ -1,8 +1,10 @@
+@inject('dialog', 'App\Services\DialogCvaService')
+
 <dialog
     wire:ignore.self
     x-effect="__dialogOpen ? $el.showModal() : $el.close()"
     x-on:cancel="__dialogOpen = false"
-    {{ $attributes->twMerge('w-full max-w-lg border bg-background p-6 shadow-lg  sm:rounded-lg transition-[translate,opacity,scale,overlay,display] [transition-behavior:allow-discrete] open:animate-in open:fade-in-0 open:zoom-in-95 animate-out duration-200 fade-out-0 zoom-out-95 backdrop:bg-black/80 backdrop:duration-300 backdrop:opacity-0 backdrop:transition-[opacity,display,overlay] backdrop:[transition-behavior:allow-discrete] open:backdrop:opacity-100 [@starting-style]:open:backdrop:opacity-0') }}
+    {{ $attributes->twMerge($dialog(['side' => 'center', 'variant' => 'dialog'])) }}
 >
     {{ $slot }}
 
